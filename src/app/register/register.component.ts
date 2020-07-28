5d10e4703d290f69de3f637290be975e072e05ef
TianleJin <tianlejin99@gmail.com>
2020-07-28 13:59:35 +1000

    Created UI for Sign Up page

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { passwordMatchValidator } from '../shared/passwordMatchValidator.directive';
+
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registrationForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    phone: ['', Validators.required],
+    userName: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    confirmPassword: ['', Validators.required]
+  }, { validator: passwordMatchValidator });
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    // Validate the username and password with the database
+    // Redirect user to Homepage
+    console.log(this.registrationForm.errors);
+    console.log(this.confirmPassword.errors);
+  }
+
+  get firstName() { return this.registrationForm.get('firstName'); }
+  get lastName() { return this.registrationForm.get('lastName'); }
+  get phone() { return this.registrationForm.get('phone'); }
+  get userName() { return this.registrationForm.get('userName'); }
+  get password() { return this.registrationForm.get('password'); }
+  get confirmPassword() { return this.registrationForm.get('confirmPassword'); }
 }
---
+++ src/app/shared/passwordMatchValidator.directive.ts
@@ -0,0 +1,8 @@
+import { ValidatorFn, FormGroup, ValidationErrors } from '@angular/forms';
+
+
+export const passwordMatchValidator: ValidatorFn = (control: FormGroup): ValidationErrors | null => {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+    return password.value === confirmPassword.value ? null: { 'passwordMismatch': true };
+};
